8e2951bb117530cfd9f09e19c6e300a2921a59a7
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-09-12 17:15:01 -0300

    ok

---
+++ logica-de-programacao/04-alunos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04_alunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alunos = new string[] {"Pedro", "João", "Vinicius", "Gabriel"};
+        
+        foreach (string aluno in alunos)
+        {
+            Console.WriteLine(aluno);
+        }
+        }
+    }
+}
---
+++ orientacao-a-objetos/03-Comportamento de Memória/Boxing/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Object obj = 10;
+            int numero = (int)obj;
+            System.Console.WriteLine(numero);
+        }
+    }
+}
