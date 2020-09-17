f421a2ec6ad32729532c3c2f4f8cc8d9e6f06315
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-09-16 23:32:09 -0300

    datetime e timespan

---
+++ 04-topicos-especiais/DateTime2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace datetime2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58, 275);
+
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day);
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year);
+
+            // Conversores de Data
+            string s1 = d.ToLongDateString();
+            string s2 = d.ToLongTimeString();
+            string s3 = d.ToShortDateString();
+            string s4 = d.ToShortTimeString();
+            string s5 = d.ToString();
+            string s6 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            string s7 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.WriteLine(s4);
+            Console.WriteLine(s5);
+            Console.WriteLine(s6);
+            Console.WriteLine(s7);
+            System.Console.WriteLine();
+
+            DateTime x = new DateTime(2000, 1, 1, 00, 00, 00, 500);
+
+            DateTime y1 = x.AddHours(3);
+            DateTime y2 = x.AddMinutes(20);
+            DateTime y3 = x.AddDays(7);
+            DateTime y4 = x.AddMonths(9);
+            DateTime y5 = x.AddYears(20);
+            DateTime y6 = x.AddSeconds(30);
+            System.Console.WriteLine(y1);
+            System.Console.WriteLine(y2);
+            System.Console.WriteLine(y3);
+            System.Console.WriteLine(y4);
+            System.Console.WriteLine(y5);
+            System.Console.WriteLine(y6);
+            System.Console.WriteLine();
+
+            DateTime data1 = new DateTime(2000, 11, 30);
+            DateTime data2 = new DateTime(2000, 8, 20);
+
+            TimeSpan diff = data1.Subtract(data2);
+            System.Console.WriteLine(diff);
+        }
+    }
+}
---
+++ 04-topicos-especiais/TimeSpan2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace timespan2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = TimeSpan.MaxValue;
+            TimeSpan t2 = TimeSpan.MinValue;
+            TimeSpan t3 = TimeSpan.Zero;
+            System.Console.WriteLine(t1);
+            System.Console.WriteLine(t2);
+            System.Console.WriteLine(t3);
+
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+
+            Console.WriteLine(t);
+            Console.WriteLine("\nDays: " + t.Days);
+            Console.WriteLine("Hours: " + t.Hours);
+            Console.WriteLine("Minutes: " + t.Minutes);
+            Console.WriteLine("Milliseconds: " + t.Milliseconds);
+            Console.WriteLine("Seconds: " + t.Seconds);
+            Console.WriteLine("Ticks: " + t.Ticks);
+
+            Console.WriteLine("TotalDays: " + t.TotalDays);
+            Console.WriteLine("TotalHours: " + t.TotalHours);
+            Console.WriteLine("TotalMinutes: " + t.TotalMinutes);
+            Console.WriteLine("TotalSeconds: " + t.TotalSeconds);
+            Console.WriteLine("TotalMilliseconds: " + t.TotalMilliseconds + "\n\n");
+
+            // Operações com TimeSpan
+
+            TimeSpan time1 = new TimeSpan(1, 30, 10);
+            TimeSpan time2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = time1.Add(time2);
+            TimeSpan dif = time1.Subtract(time2);
+            TimeSpan mult = time2.Multiply(2.0);
+            TimeSpan div = time2.Divide(2.0);
+
+            Console.WriteLine(time1);
+            Console.WriteLine(time2);
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
+        }
+    }
+}
