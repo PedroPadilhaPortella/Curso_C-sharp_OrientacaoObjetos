d2aa24255af109cf44d6d5d36978013d5b1a0867
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-09-17 23:49:50 -0300

    datetimekind e ISO

---
+++ 04-topicos-especiais/DateTimeKind/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace datetimekind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = new DateTime(2020, 09, 17, 23, 15, 20, DateTimeKind.Local);
+            DateTime d2 = new DateTime(2020, 09, 17, 23, 15, 20, DateTimeKind.Utc);
+            DateTime d3 = new DateTime(2020, 09, 17, 23, 15, 20, DateTimeKind.Unspecified);
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Kind);//data local, que pode ser convertida em utc
+            Console.WriteLine($"LocalTime: {d1.ToLocalTime()}");
+            Console.WriteLine($"UniversalTime {d1.ToUniversalTime()}");
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d2.Kind);//data utc, que pode ser convertida em local
+            Console.WriteLine($"LocalTime: {d2.ToLocalTime()}");
+            Console.WriteLine($"UniversalTime {d2.ToUniversalTime()}");
+
+            Console.WriteLine(d3);
+            Console.WriteLine(d3.Kind); //data não especificada, então pode ser convertida para local e utc
+            Console.WriteLine($"LocalTime: {d3.ToLocalTime()}");
+            Console.WriteLine($"UniversalTime {d3.ToUniversalTime()}");
+        }
+    }
+}
---
+++ 04-topicos-especiais/ISO8601/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace iso8601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Padrão ISO 8601 -> yyyy-MM-ddTHH:mm:ssZ(Z de utc)
+
+            DateTime data1 = DateTime.Parse("2000-08-15 13:10:15");
+            DateTime data2 = DateTime.Parse("2000-08-15T13:10:15Z"); 
+            //Ira formatar essa data para o padrão Local, subtraindo 3 horas
+
+            Console.WriteLine(data1);
+            Console.WriteLine(data1.Kind);//data local, que pode ser convertida em utc
+            Console.WriteLine($"LocalTime: {data1.ToLocalTime()}");
+            Console.WriteLine($"UniversalTime {data1.ToUniversalTime()}");
+
+            Console.WriteLine(data2);
+            Console.WriteLine(data2.Kind);//data utc, que pode ser convertida em local
+            Console.WriteLine($"LocalTime: {data2.ToLocalTime()}");
+            Console.WriteLine($"UniversalTime {data2.ToUniversalTime()}");
+
+            Console.WriteLine(data2.ToUniversalTime().ToString("yyyy-MM-ddYHH:mm:ssZ"));
+            //Converte pra o formato universal e depois para string, que pode ser usada em apis, json, etc;
+        }
+    }
+}
