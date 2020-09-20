fe8dc1d01e6fa8956cc063a2f3e18a30c2deefd0
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-09-20 02:48:46 -0300

    enum, entities e composicao

---
+++ 05-enumeracao-composicao/OrderEnum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OrderEnum.Entities;
+using OrderEnum.Entities.Enums;
+
+namespace OrderEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarando uma classe que tem um atributo do Tipo OrderStatus, que é um Enum
+            Order order = new Order
+            {
+                Id = 4930,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            System.Console.WriteLine(order);
+
+            string texto = OrderStatus.PendingPayment.ToString(); //Converter Enum para String
+            System.Console.WriteLine(texto);
+
+            OrderStatus osx = Enum.Parse<OrderStatus>("Delivered"); //Converte String para Enum
+            System.Console.WriteLine(osx);
+
+            //Outras formas de converter String para Enum do tipo OrderStatus
+
+            // OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            //OrderStatus os = (OrderStatus)Enum.Parse(typeof(OrderStatus), "Delivered");
+
+            //OrderStatus os;
+            //Enum.TryParse("Delivered", out os);
+        }
+    }
+}
