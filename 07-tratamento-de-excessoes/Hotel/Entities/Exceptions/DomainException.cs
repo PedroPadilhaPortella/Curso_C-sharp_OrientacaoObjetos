765a1149fc1b58be0aec71c288f2cbe642b69a43
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-05 23:34:39 -0300

    tratamento de excessoes hotel

---
+++ 07-tratamento-de-excessoes/Hotel/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Hotel.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message){}
+    }
+}
---
+++ 07-tratamento-de-excessoes/Hotel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Hotel.Entities;
+using Hotel.Entities.Exceptions;
+
+namespace Hotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reserva = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine(reserva);
+
+                Console.WriteLine("\nEnter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reserva.UpdateDates(checkIn, checkOut);
+                Console.WriteLine(reserva);
+            }
+            catch (DomainException err)
+            {
+                Console.WriteLine("Error in Reservation: " + err.Message);
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine("Format Error: " + err.Message);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("General Error: " + err.Message);
+            }
+        }
+    }
+}
