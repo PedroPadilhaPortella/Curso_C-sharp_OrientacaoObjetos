800c30f69352be6f5d39aa4851bc8a8f693ad5a0
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-14 17:01:45 -0300

    file e fileinfo

---
+++ 8-trabalhando-com-arquivos/1-File_FileInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _1_File_FileInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\pedro\Documents\GitHub\Curso_C-sharp_OrientacaoObjetos\8-trabalhando-com-arquivos\1-File_FileInfo\teste.txt";
+            string targetPath = @"C:\Users\pedro\Documents\GitHub\Curso_C-sharp_OrientacaoObjetos\8-trabalhando-com-arquivos\1-File_FileInfo\teste2.txt";
+
+            try{
+                FileInfo fileinfo = new FileInfo(sourcePath);
+                fileinfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                System.Console.WriteLine("Arquivo copiado!");
+                foreach (string line in lines){
+                    System.Console.WriteLine(line);
+                }
+
+            }catch(IOException e){
+                System.Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
