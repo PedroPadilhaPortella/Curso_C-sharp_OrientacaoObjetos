753ddcf9544b9ef2f24469935b737e2178cddd79
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-21 23:37:35 -0300

    exercicio

---
+++ 10-generics-set-dictionary/CursosOnline/Entities/Students.cs
@@ -0,0 +1,24 @@
+namespace CursosOnline.Entities
+{
+    class Students
+    {
+        public int Id { get; set; }
+        public Students(int id){
+            Id = id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+        
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Students)) {
+                return false;
+            }
+            Students other = obj as Students;
+            return Id.Equals(other.Id);
+        }
+    }
+}
---
+++ 10-generics-set-dictionary/CursosOnline/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CursosOnline.Entities;
+
+namespace CursosOnline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Students> estudantes = new HashSet<Students>();
+            int id = 0;
+            Console.Write("How many students from course A: ");
+            int a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++) {
+                id = int.Parse(Console.ReadLine());
+                estudantes.Add(new Students(id));
+            }
+
+            Console.Write("How many students from course B: ");
+            a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++) {
+                id = int.Parse(Console.ReadLine());
+                estudantes.Add(new Students(id));
+            }
+
+            Console.Write("How many students from course C: ");
+            a = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++) {
+                id = int.Parse(Console.ReadLine());
+                estudantes.Add(new Students(id));
+            }
+
+            System.Console.WriteLine($"Total Students: {estudantes.Count}");
+        }
+    }
+}
