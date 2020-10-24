3b511e90c93a1ef7312ba6bad6a46cb5d9ef4a2f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-23 22:29:15 -0300

    exercicio dictionary

--- 10-generics-set-dictionary/exercicios/Votacao/Program.cs
+++ 10-generics-set-dictionary/exercicios/Votacao/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Votacao
 {
@@ -6,7 +8,34 @@ namespace Votacao
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter full path: ");
+            string path = Console.ReadLine();
+
+            try{
+                Dictionary<string, int> dados = new Dictionary<string, int>();
+
+                using(StreamReader sr = File.OpenText(path)) {
+
+                    while(!sr.EndOfStream) {
+                        string[] votingRecord = sr.ReadLine().Split(",");
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+                        
+                        if(dados.ContainsKey(candidate)){
+                            dados[candidate] += votes;
+                        }else{
+                            dados[candidate] = votes;
+                        }
+                    }
+                }
+
+                foreach (var item in dados) {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
