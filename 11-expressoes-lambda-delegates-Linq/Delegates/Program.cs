8ef403e22a7e38f57431af4dd5716f3b1bd4e7d5
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-24 17:50:28 -0300

    delegates

---
+++ 11-expressoes-lambda-delegates-Linq/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Delegates
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    delegate double UnaryNumericOperation(double n);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            //Delegates
+            BinaryNumericOperation soma = CalculationService.Sum;
+            BinaryNumericOperation maior = new BinaryNumericOperation(CalculationService.Max);
+            UnaryNumericOperation quadrado = CalculationService.Square;
+
+            System.Console.WriteLine("Soma: " + soma(a, b));
+            System.Console.WriteLine("Maior: " + maior.Invoke(a, b));
+            System.Console.WriteLine("Quadrado: " + quadrado(b));
+        }
+    }
+}
