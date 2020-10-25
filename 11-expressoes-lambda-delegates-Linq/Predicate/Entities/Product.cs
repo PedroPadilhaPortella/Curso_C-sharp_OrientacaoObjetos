989d6572bfa68595d080b4045c605a780afec3af
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-24 22:18:34 -0300

    predicate

---
+++ 11-expressoes-lambda-delegates-Linq/Predicate/Entities/Product.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+
+namespace Predicate.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price){
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, R${Price.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
