ce977297dd9d2f01eaf185ae5f908648797bebcb
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-25 22:54:46 -0300

    fibonnaci

---
+++ 01-logica-de-programacao/05-Fibonnaci/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_Fibonnaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            Console.Write("Quantos valores da Sequencia de Fibonnaci serão impressos: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Sequencia:");
+
+            for (int i = 0; i < n; i++) {
+                Console.Write($"  {a}");
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
