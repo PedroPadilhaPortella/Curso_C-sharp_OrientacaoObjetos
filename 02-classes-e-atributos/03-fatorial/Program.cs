7faa71ac3abed88b89d12d3c3160ce200518c2c5
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-10-28 22:20:49 -0300

    fatorial

---
+++ 02-classes-e-atributos/03-fatorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _03_fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Calcule um fatorial: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.Write($"O fatorial de {i} é {Fatorial(i)}");
+            
+        }
+
+        static int Fatorial(int num)
+        {
+            if(num <= 0){
+                return 1;
+            }
+            return num * Fatorial(num - 1);
+        }
+    }
+}
