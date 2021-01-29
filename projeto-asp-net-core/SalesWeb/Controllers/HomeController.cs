4d8bd0fa6f9f210c7da0d2ecd90d4794a8b26c1f
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-01-29 16:45:39 -0300

    First controller and Razor pages tests

--- projeto-asp-net-core/SalesWeb/Controllers/HomeController.cs
+++ projeto-asp-net-core/SalesWeb/Controllers/HomeController.cs
@@ -20,6 +20,8 @@ namespace SalesWeb.Controllers
 
         public IActionResult Index()
         {
+            ViewData["Message"] = "Sales Web MVC";
+            ViewData["Professor"] = "Nelio Alves";
             return View();
         }
 
